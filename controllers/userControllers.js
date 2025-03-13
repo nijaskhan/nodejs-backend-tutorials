358aea6d61ae99e34b64bbd25f00e724ec2de20d
Nijas Khan <khan@parelstudio.com>
2025-03-13 21:41:31 +0530

    last session: integration with frontend - finished

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -184,7 +184,7 @@ module.exports = {
                             statusCode: 200,
                             message: "User Login successfully",
                             token: token,
-                            user: userFound
+                            data: userFound
                         });
                     } else {
                         return res.status(200).json({
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = require('./routes');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
+
+app.use(cors());
 
 const dbConfig = require('./dbConfig');
 
